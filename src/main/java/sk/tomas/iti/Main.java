0772e8340864a470c01ddedced4677bc8f3e7bdd
Tomáš Páchnik <tomas.pachnik@gmail.com>
2018-09-23 16:18:56 +0200

    Servant init

---
+++ src/main/java/sk/tomas/iti/Main.java
@@ -0,0 +1,11 @@
+package sk.tomas.iti;
+
+
+import sk.tomas.servant.core.Servant;
+
+public class Main {
+
+    public static void main(String [] args){
+        Servant.scanPackage("sk.tomas.iti");
+    }
+}
---
+++ src/test/java/sk/tomas/iti/LibraryTest.java
@@ -0,0 +1,14 @@
+package sk.tomas.iti;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+
+    @Test
+    public void testSomeLibraryMethod() {
+        assertTrue(true);
+    }
+
+}
